4c0c2f578467cbd0ac1270eb8368c7d366f03e25
ericcapiz <ericcapiz@gmail.com>
2021-09-02 16:14:22 -0500

    started dashboard area

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import Icon from '../Icons/Icon';
+import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
     return (
-        <div>
-            dashboard
-        </div>
+        <Fragment>
+            <header className="d-flex align-items-center">
+                <Icon icon='dashboard' width={20} />
+                <h5 className="mb-0 ml-2">Sales Dashboard</h5>
+            </header>
+            <div className={styles.grid}>
+
+            </div>
+        </Fragment>
     )
 }
 
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -7,7 +7,7 @@ import styles from './TopNav.module.css';
 const TopNav = () => {
     return (
         <nav className={`${styles.topNav} d-flex justify-content-between align-items-center px-3`}>
-            <h5 className={styles.brand}>Dashboard</h5>
+            <h5 className={styles.brand}>Sales Dashboard</h5>
             <SearchBar />
         </nav>
     )
