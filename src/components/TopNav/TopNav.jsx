d5850729f49fc403ebedf9f75b6e649f4ca29f7f
ericcapiz <ericcapiz@gmail.com>
2021-09-02 12:57:56 -0500

    first commit

---
+++ src/components/TopNav/TopNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './TopNav.module.css';
+
+
+
+const TopNav = () => {
+    return (
+        <div>
+            TopNav
+        </div>
+    )
+}
+
+export default TopNav
